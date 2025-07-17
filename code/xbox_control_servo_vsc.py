0fe5125196c75009a67295c5c00ceffe2850db44
Fab1e2000 <31071325781@qq.com>
2025-07-17 16:26:40 +0800

    反馈停止

--- code/xbox_control_servo_vsc.py
+++ code/xbox_control_servo_vsc.py
@@ -2,7 +2,7 @@ import pygame
 import serial
 import time
 
-control = serial.Serial('COM7',9600)
+control = serial.Serial('COM5',9600)
 time.sleep(2)
 
 pygame.init()
--- code/xbox_servo_control_ide/xbox_servo_control_ide.ino
+++ code/xbox_servo_control_ide/xbox_servo_control_ide.ino
@@ -1,24 +1,48 @@
 #include<Servo.h>
 Servo servo;
-int minAngle = 0;
-int maxAngle = 160;
+const int minAngle = 0;
+const int maxAngle = 160;
+
+const int fsrPin = A0;
+const int fsrPressure = 700;
+
+int currentAngle = 0;
+bool locked = false;
 
 void setup() {
   Serial.begin(9600);
   servo.attach(3);
   servo.write(0);
-  Serial.println("servo initiated");
+  //Serial.println("initiated");
 }
 
-void loop() {
+void loop(){
   if(Serial.available()){
+    int pressure = analogRead(fsrPin);
+    //Serial.print("pressure is ");
+    //Serial.println(pressure);
+
     String input = Serial.readStringUntil('\n');
-    int angle = input.toInt();
-    if(angle>=minAngle&&angle<=maxAngle){
-      servo.write(angle);
-      Serial.println("instant angle is:");
-      Serial.println(angle);
+    int targetAngle = input.toInt();
+    //Serial.print("target angle is ");
+    //Serial.println(targetAngle);
+
+    if(pressure <= fsrPressure){
+    //Serial.println("safe, moving to target angle");
+    servo.write(targetAngle);
+    currentAngle = targetAngle;
+    }
+      else{
+     if(targetAngle < currentAngle){
+        //Serial.println("release command");
+        servo.write(targetAngle);
+        currentAngle = targetAngle;
+      }
+      else{
+        //Serial.println("danger, reject command");
+        currentAngle = currentAngle;
+      }
     }
   }
+  delay(100);
 }
-
